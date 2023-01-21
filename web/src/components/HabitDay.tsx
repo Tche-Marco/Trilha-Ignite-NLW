7a8bd31b0758e0daba347bfd46adbb62ec4db8d8
Marco Antônio <marcomartins06@rede.ulbra.br>
2023-01-20 23:36:01 -0300

    adicionando elementos ao Front-end

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,9 +1,33 @@
-// interface HabitDayProps {
-//   completed: number
-// }
+import * as Popover from '@radix-ui/react-popover'
+import clsx from 'clsx'
+import { ProgressBar } from './ProgressBar'
 
-export function HabitDay() {
+interface HabitDayProps {
+  completed: number
+  amount: number
+}
+
+export function HabitDay({ completed, amount }: HabitDayProps) {
+  const completedPercentage = Math.round((completed / amount) * 100)
+
   return (
-    <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+    <Popover.Root>
+      <Popover.Trigger
+        className={clsx(
+          'w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg',
+          { 'bg-violet-500': completedPercentage >= 80 },
+        )}
+      />
+      <Popover.Portal>
+        <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
+          <span className="font-semibold text-zinc-400">terça-feira</span>
+          <span className="mt-1 font-extrabold leading-tight text-3xl">
+            17/01
+          </span>
+          <ProgressBar progress={completedPercentage} />
+          <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
   )
 }
